b3377d04dfe558b519ae3076608302dfa659cc93
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-05-01 12:13:11 +0700

    fix warning

--- src/main/java/kk/kertaskerja/edge_service/config/SecurityConfig.java
+++ src/main/java/kk/kertaskerja/edge_service/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
