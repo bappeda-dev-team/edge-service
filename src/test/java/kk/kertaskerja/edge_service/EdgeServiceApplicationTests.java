f9537fa1a9de8f91715773d316769386d3749e1c
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-05-01 13:49:37 +0700

    fix test

--- src/test/java/kk/kertaskerja/edge_service/EdgeServiceApplicationTests.java
+++ src/test/java/kk/kertaskerja/edge_service/EdgeServiceApplicationTests.java
@@ -2,8 +2,10 @@ package kk.kertaskerja.edge_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -22,10 +24,13 @@ class EdgeServiceApplicationTests {
 			new GenericContainer<>(DockerImageName.parse("redis:alpine"))
 			.withExposedPorts(REDIS_PORT);
 
+	@MockitoBean
+	ReactiveClientRegistrationRepository clientRegistrationRepository;
+
 	@DynamicPropertySource
 	static void redisProperties(DynamicPropertyRegistry registry) {
 		registry.add("spring.redis.host", () -> redis.getHost());
-		registry.add("spring.redis.port", () -> REDIS_PORT);
+		registry.add("spring.redis.port", () -> redis.getMappedPort(REDIS_PORT));
 	}
 
 	@Test
