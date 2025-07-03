85f9f46a6f1e2ad20086f6b9946e731ba50ddfc7
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-07-03 13:08:27 +0700

    update xx

--- src/main/java/kk/kertaskerja/edge_service/config/SecurityConfig.java
+++ src/main/java/kk/kertaskerja/edge_service/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
     CorsWebFilter corsWebFilter(CorsProperties corsProperties) {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(corsProperties.getAllowedOrigins());
+        config.setAllowedOrigins(corsProperties.getAllowedOrigins());
         config.setAllowedHeaders(corsProperties.getAllowedHeaders());
         config.setAllowedMethods(corsProperties.getAllowedMethods());
 
