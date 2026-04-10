d1bc43229fef766a4c0fa9724a19a575a23f0cb4
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2026-04-10 19:54:23 +0700

    cookie if no xsessionid

--- src/main/java/kk/kertaskerja/edge_service/config/SessionAuthenticationConverter.java
+++ src/main/java/kk/kertaskerja/edge_service/config/SessionAuthenticationConverter.java
@@ -1,5 +1,6 @@
 package kk.kertaskerja.edge_service.config;
 
+import org.springframework.http.HttpCookie;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
@@ -9,7 +10,21 @@ import reactor.core.publisher.Mono;
 public class SessionAuthenticationConverter implements ServerAuthenticationConverter {
     @Override
     public Mono<Authentication> convert(ServerWebExchange exchange) {
+        // try from header
         String sessionId = exchange.getRequest().getHeaders().getFirst("X-Session-Id");
+
+        // Fallback ke cookie kalau header tidak ada
+        if (sessionId == null || sessionId.isBlank()) {
+            HttpCookie cookie = exchange.getRequest()
+                    .getCookies()
+                    .getFirst("sessionId");
+
+            if (cookie != null) {
+                sessionId = cookie.getValue();
+            }
+        }
+
+        // Kalau tetap tidak ada → tidak authenticate
         if (sessionId == null || sessionId.isBlank()) {
             return Mono.empty();
         }
